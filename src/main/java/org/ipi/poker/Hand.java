97f31643f53e0db8a333523db0b001f157aeca31
Merokai <amonyak@amostack.ovh>
2020-05-07 15:39:25 +0200

    Handles one and two pairs

--- src/main/java/org/ipi/poker/Hand.java
+++ src/main/java/org/ipi/poker/Hand.java
@@ -1,8 +1,8 @@
 package org.ipi.poker;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Hand {
 
@@ -22,13 +22,35 @@ public class Hand {
     }
 
     public boolean isBetterThan(Hand hand2) {
-        Optional<Integer> hand1Max = cards.stream().map(Card::getScore).max(Integer::compare);
-        Optional<Integer> hand2Max = hand2.cards.stream().map(Card::getScore).max(Integer::compare);
+        return computeScore() > hand2.computeScore();
+    }
 
-        if (hand1Max.isEmpty() || hand2Max.isEmpty()) {
-            throw new IllegalArgumentException();
+    private int computeScore() {
+        Set<Card> mutableCards = new HashSet<>(cards);
+
+        int score = 0;
+
+        // Computing Pairs
+        // 20 to 140 for One Pair + High Card
+        // 200 to 1400 for Two Pair
+        Set<Integer> uniqueScores = mutableCards.stream().map(Card::getScore).collect(Collectors.toSet());
+        int pairScore = 0;
+        for (int cardScore : uniqueScores) {
+            if (mutableCards.stream().filter(c -> c.getScore() == cardScore).count() == 2) {
+                // This is a second Pair
+                if (pairScore > 0) {
+                    return Math.max(pairScore, cardScore) * 100;
+                }
+                pairScore = cardScore;
+                mutableCards = mutableCards.stream().filter(c -> c.getScore() != cardScore).collect(Collectors.toSet());
+            }
         }
+        score += 10 * pairScore;
 
-        return hand1Max.get() > hand2Max.get();
+        // Computing High Card value
+        // 2 to 14 score
+        score += mutableCards.stream().map(Card::getScore).max(Integer::compare).orElse(0);
+
+        return score;
     }
 }
--- src/test/java/org/ipi/poker/HandTest.java
+++ src/test/java/org/ipi/poker/HandTest.java
@@ -20,17 +20,95 @@ public class HandTest {
     }
 
     @Test void compareHands(){
+        // Hand 1 wins with a High Card of 8
         Hand hand1 = new Hand();
+        hand1.addCard(new Card(Pip.HEARTS, 8));
         Hand hand2 = new Hand();
-
-        hand1.addCard(new Card(Pip.HEARTS, 5));
-        hand2.addCard(new Card(Pip.HEARTS, 4));
+        hand2.addCard(new Card(Pip.HEARTS, 7));
 
         assertTrue(hand1.isBetterThan(hand2));
         assertFalse(hand2.isBetterThan(hand1));
 
+        // Hand 1 wins with a High Card of 1
+        hand1 = new Hand();
+        hand1.addCard(new Card(Pip.HEARTS, 1));
+        hand2 = new Hand();
+        hand2.addCard(new Card(Pip.HEARTS, 13));
+
+        assertTrue(hand1.isBetterThan(hand2));
+        assertFalse(hand2.isBetterThan(hand1));
+
+        // Hand 1 wins with a pair of 2 against an High Card
+        hand1 = new Hand();
+        hand1.addCard(new Card(Pip.HEARTS, 2));
+        hand1.addCard(new Card(Pip.CLOVERS, 2));
+        hand2 = new Hand();
         hand2.addCard(new Card(Pip.HEARTS, 1));
+
+        assertTrue(hand1.isBetterThan(hand2));
+        assertFalse(hand2.isBetterThan(hand1));
+
+
+        // Hand 1 wins with a pair of 3 against a pair of 2
+        hand1 = new Hand();
+        hand1.addCard(new Card(Pip.HEARTS, 3));
+        hand1.addCard(new Card(Pip.CLOVERS, 3));
+        hand2 = new Hand();
+        hand2.addCard(new Card(Pip.HEARTS, 2));
+        hand2.addCard(new Card(Pip.PIKES, 2));
+
+        assertTrue(hand1.isBetterThan(hand2));
+        assertFalse(hand2.isBetterThan(hand1));
+
+
+        // Hand 1 wins with a pair thanks to his high card
+        hand1 = new Hand();
+        hand1.addCard(new Card(Pip.HEARTS, 3));
+        hand1.addCard(new Card(Pip.CLOVERS, 3));
+        hand1.addCard(new Card(Pip.CLOVERS, 6));
+        hand2 = new Hand();
+        hand2.addCard(new Card(Pip.TILES, 3));
+        hand2.addCard(new Card(Pip.PIKES, 3));
+        hand2.addCard(new Card(Pip.PIKES, 5));
+
+        assertTrue(hand1.isBetterThan(hand2));
+        assertFalse(hand2.isBetterThan(hand1));
+
+
+        // Hand 1 wins with two pairs
+        hand1 = new Hand();
+        hand1.addCard(new Card(Pip.HEARTS, 3));
+        hand1.addCard(new Card(Pip.CLOVERS, 3));
+        hand1.addCard(new Card(Pip.CLOVERS, 2));
+        hand1.addCard(new Card(Pip.TILES, 2));
+        hand2 = new Hand();
+        hand2.addCard(new Card(Pip.TILES, 10));
+        hand2.addCard(new Card(Pip.PIKES, 10));
+        hand2.addCard(new Card(Pip.PIKES, 5));
+        hand2.addCard(new Card(Pip.PIKES, 4));
+
+        assertTrue(hand1.isBetterThan(hand2));
+        assertFalse(hand2.isBetterThan(hand1));
+
+
+        // nobody wins with two pairs
+        hand1 = new Hand();
+        hand1.addCard(new Card(Pip.HEARTS, 4));
+        hand1.addCard(new Card(Pip.CLOVERS, 4));
+        hand1.addCard(new Card(Pip.CLOVERS, 2));
+        hand1.addCard(new Card(Pip.TILES, 2));
+        hand2 = new Hand();
+        hand2.addCard(new Card(Pip.TILES, 4));
+        hand2.addCard(new Card(Pip.PIKES, 4));
+        hand2.addCard(new Card(Pip.CLOVERS, 3));
+        hand2.addCard(new Card(Pip.PIKES, 3));
+
         assertFalse(hand1.isBetterThan(hand2));
-        assertTrue(hand2.isBetterThan(hand1));
+        assertFalse(hand2.isBetterThan(hand1));
+
+
+
     }
+
+
 }
