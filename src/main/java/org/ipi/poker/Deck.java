df424702cd8a9b6eea09b9dd3e50c7233ccebe9b
Merokai <amonyak@amostack.ovh>
2020-05-07 10:21:18 +0200

    Instantiating a deck

---
+++ src/main/java/org/ipi/poker/Deck.java
@@ -0,0 +1,4 @@
+package org.ipi.poker;
+
+public class Deck {
+}
---
+++ src/test/java/org/ipi/poker/DeckTest.java
@@ -0,0 +1,13 @@
+package org.ipi.poker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeckTest {
+
+    @Test void newDeckHasCards(){
+        Deck deck = new Deck();
+    }
+
+}
