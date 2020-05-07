a453fd4191d37adf4e83cc8c98d2c3d77941e2a0
Merokai <amonyak@amostack.ovh>
2020-05-07 10:06:13 +0200

    Card has a Pip

--- src/test/java/org/ipi/poker/CardTest.java
+++ src/test/java/org/ipi/poker/CardTest.java
@@ -2,6 +2,7 @@ package org.ipi.poker;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CardTest {
@@ -10,5 +11,10 @@ public class CardTest {
     void hasPip() {
         Card card = new Card(Pip.CLOVERS, 4);
         assertNotNull(card.getPip());
+
+        assertEquals(Pip.CLOVERS, new Card(Pip.CLOVERS, 4).getPip());
+        assertEquals(Pip.PIKES, new Card(Pip.PIKES, 4).getPip());
+        assertEquals(Pip.HEARTS, new Card(Pip.HEARTS, 4).getPip());
+        assertEquals(Pip.TILES, new Card(Pip.TILES, 4).getPip());
     }
 }
